12326ebce0bdabf1410753c4ed695f31a8e51d7e
Matthias Etchegaray <matthias@happywait.com>
2021-12-30 22:05:18 +0100

    Added mock tests

--- src/modules/home/screens/Home.test.ts
+++ src/modules/home/screens/Home.test.ts
@@ -9,11 +9,41 @@ describe("testing api", () => {
     fetch.resetMocks();
   });
 
-  it("calls google and returns data to me", async () => {
-    fetch.mockResponseOnce(JSON.stringify({ data: "12345" }));
+  it("calls vimeo video and returns data to me", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        account_type: "business",
+        author_name: "Mouvement Colibris",
+        author_url: "https://vimeo.com/colibris",
+        description:
+          "En 3', voici comment mettre en ligne votre vidéo \"(R)êve d'école\" !",
+        duration: 211,
+        height: 270,
+        html: '<iframe src="https://player.vimeo.com/video/74918975?h=b2f1716794&amp;app_id=122963" width="480" height="270" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Comment mettre en ligne votre vid&amp;eacute;o"></iframe>',
+        is_plus: "0",
+        provider_name: "Vimeo",
+        provider_url: "https://vimeo.com/",
+        thumbnail_height: 166,
+        thumbnail_url:
+          "https://i.vimeocdn.com/video/449365291-86b78f2542d9b13c68fe6634e6f0e55db55b0e515fa3686820895c8b948c373a-d_295x166",
+        thumbnail_url_with_play_button:
+          "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F449365291-86b78f2542d9b13c68fe6634e6f0e55db55b0e515fa3686820895c8b948c373a-d_295x166&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png",
+        thumbnail_width: 295,
+        title: "Comment mettre en ligne votre vidéo",
+        type: "video",
+        upload_date: "2013-09-19 06:08:41",
+        uri: "/videos/74918975",
+        url: "https://vimeo.com/74918975",
+        version: "1.0",
+        video_id: 74918975,
+        width: 480,
+      })
+    );
 
     //assert on the response
-    const res = await doFetch<ApiResponse>({ url: "https://vimeo.com/74918975" });
+    const res = await doFetch<ApiResponse>({
+      url: "https://vimeo.com/74918975",
+    });
     expect(res).toEqual({
       account_type: "business",
       author_name: "Mouvement Colibris",
@@ -44,6 +74,76 @@ describe("testing api", () => {
 
     //assert on the times called and arguments given to fetch
     expect(fetch.mock.calls.length).toEqual(1);
-    // expect(fetch.mock.calls[0][0]).toEqual('https://google.com')
+    expect(fetch.mock.calls[0][0]).toEqual(
+      "http://noembed.com/embed?url=https://vimeo.com/74918975"
+    );
+  });
+
+  it("calls flickr image and returns data to me", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        author_name: "Pierre Metivier",
+        author_url: "https://www.flickr.com/photos/feuilllu/",
+        cache_age: 3600,
+        flickr_type: "photo",
+        height: 685,
+        html: '<a data-flickr-embed="true" href="https://www.flickr.com/photos/feuilllu/45771361701/" title="2018 Visite de Klaxoon by Pierre Metivier, on Flickr"><img alt="2018 Visite de Klaxoon" height="685" src="https://noembed.com/i/https://live.staticflickr.com/4817/45771361701_2678123510_b.jpg" width="1024"></a><script async src="https://embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>',
+        license: "Attribution-NonCommercial License",
+        license_id: "2",
+        license_url: "https://creativecommons.org/licenses/by-nc/2.0/",
+        media_url:
+          "https://live.staticflickr.com/4817/45771361701_2678123510_b.jpg",
+        provider_name: "Flickr",
+        provider_url: "https://www.flickr.com/",
+        thumbnail_height: 150,
+        thumbnail_url:
+          "https://live.staticflickr.com/4817/45771361701_2678123510_q.jpg",
+        thumbnail_width: 150,
+        title: "2018 Visite de Klaxoon",
+        type: "photo",
+        url: "https://www.flickr.com/photos/feuilllu/45771361701/",
+        version: "1.0",
+        web_page: "https://www.flickr.com/photos/feuilllu/45771361701/",
+        web_page_short_url: "https://flic.kr/p/2cJEcbD",
+        width: 1024,
+      })
+    );
+
+    //assert on the response
+    const res = await doFetch<ApiResponse>({
+      url: "https://www.flickr.com/photos/feuilllu/45771361701/",
+    });
+    expect(res).toEqual({
+      author_name: "Pierre Metivier",
+      author_url: "https://www.flickr.com/photos/feuilllu/",
+      cache_age: 3600,
+      flickr_type: "photo",
+      height: 685,
+      html: '<a data-flickr-embed="true" href="https://www.flickr.com/photos/feuilllu/45771361701/" title="2018 Visite de Klaxoon by Pierre Metivier, on Flickr"><img alt="2018 Visite de Klaxoon" height="685" src="https://noembed.com/i/https://live.staticflickr.com/4817/45771361701_2678123510_b.jpg" width="1024"></a><script async src="https://embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>',
+      license: "Attribution-NonCommercial License",
+      license_id: "2",
+      license_url: "https://creativecommons.org/licenses/by-nc/2.0/",
+      media_url:
+        "https://live.staticflickr.com/4817/45771361701_2678123510_b.jpg",
+      provider_name: "Flickr",
+      provider_url: "https://www.flickr.com/",
+      thumbnail_height: 150,
+      thumbnail_url:
+        "https://live.staticflickr.com/4817/45771361701_2678123510_q.jpg",
+      thumbnail_width: 150,
+      title: "2018 Visite de Klaxoon",
+      type: "photo",
+      url: "https://www.flickr.com/photos/feuilllu/45771361701/",
+      version: "1.0",
+      web_page: "https://www.flickr.com/photos/feuilllu/45771361701/",
+      web_page_short_url: "https://flic.kr/p/2cJEcbD",
+      width: 1024,
+    });
+
+    //assert on the times called and arguments given to fetch
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0]).toEqual(
+      "http://noembed.com/embed?url=https://www.flickr.com/photos/feuilllu/45771361701/"
+    );
   });
 });
--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,26 +1,57 @@
-export type ApiResponse = Partial<{
-    error: string
-    message: string
-    account_type: string
-    author_name: string
-    author_url: string
-    description: string
-    duration: number
-    height: number
-    html: string
-    is_plus: string
-    provider_name: string
-    provider_url: string
-    thumbnail_height: number
-    thumbnail_url: string
-    thumbnail_url_with_play_button: string
-    thumbnail_width: number
-    title: string
-    type: string
-    upload_date: Date
-    uri: string
-    url: string
-    version: string
-    video_id: number
-    width: number
-}>
+export type VimeoResponse = {
+  message: string;
+  account_type: string;
+  author_name: string;
+  author_url: string;
+  description: string;
+  duration: number;
+  height: number;
+  html: string;
+  is_plus: string;
+  provider_name: string;
+  provider_url: string;
+  thumbnail_height: number;
+  thumbnail_url: string;
+  thumbnail_url_with_play_button: string;
+  thumbnail_width: number;
+  title: string;
+  type: string;
+  upload_date: Date;
+  uri: string;
+  url: string;
+  version: string;
+  video_id: number;
+  width: number;
+};
+
+export type FlickrResponse = {
+  author_name: string;
+  author_url: string;
+  cache_age: number;
+  flickr_type: string;
+  height: number;
+  html: string;
+  license: string;
+  license_id: string;
+  license_url: string;
+  media_url: string;
+  provider_name: string;
+  provider_url: string;
+  thumbnail_height: string;
+  thumbnail_url: string;
+  thumbnail_width: number;
+  title: string;
+  type: string;
+  url: string;
+  version: string;
+  web_page: string;
+  web_page_short_url: string;
+  width: number;
+};
+
+export type ApiResponse =
+  | {
+      error?: string;
+    }
+  | FlickrResponse
+  | VimeoResponse;
